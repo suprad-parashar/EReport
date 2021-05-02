4c91b20f88508532c30a241449d85470cb90bc49
SrivalliSB <srivallisb@thinklink.io>
2021-05-03 01:08:05 +0530

    Add Splash Screen and change the UI of Login page

---
+++ app/src/main/java/com/sjbit/ereport/auth/Splashscreen.java
@@ -0,0 +1,54 @@
+package com.sjbit.ereport.auth;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.sjbit.ereport.R;
+import com.sjbit.ereport.main.HomeActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class Splashscreen extends AppCompatActivity {
+
+    private static int SPLASH_TIMER=4000;
+    //variables
+    ImageView backgroundImage;
+
+    //Animations
+    Animation sideAnim;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splashscreen);
+
+        //Hooks
+        backgroundImage=findViewById(R.id.splash_background_image);
+
+        //Animations
+        sideAnim = AnimationUtils.loadAnimation(this,R.anim.side_anim);
+
+        //set animations on elements
+        backgroundImage.setAnimation(sideAnim);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(Splashscreen.this,LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },SPLASH_TIMER);
+    }
+}

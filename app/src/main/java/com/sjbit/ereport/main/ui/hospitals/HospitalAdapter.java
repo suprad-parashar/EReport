dd921ada83858fd795847b161d9ab66adb10a1eb
sandy13521 <sandeepns@thinklink.io>
2021-06-20 13:19:12 +0530

    Fetch Hospitals from Firebase

--- app/src/main/java/com/sjbit/ereport/main/ui/hospitals/HospitalAdapter.java
+++ app/src/main/java/com/sjbit/ereport/main/ui/hospitals/HospitalAdapter.java
@@ -10,41 +10,43 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.sjbit.ereport.R;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+
 /**
  * Custom RecyclerView Adapter to set Hospital Data to corresponding View.
  */
 public class HospitalAdapter extends RecyclerView.Adapter<HospitalAdapter.HospitalViewHolder> {
 
-	String[] hospitals = {
-			"Apollo",
-			"Fortis",
-			"BGS Gleneagles Hospital",
-			"Columbia Asia"
-	};
+    ArrayList<String> hospitals = new ArrayList<>();
 
-	@NonNull
-	@Override
-	public HospitalViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-		View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.view_hospitals, parent, false);
-		return new HospitalViewHolder(view);
-	}
+    HospitalAdapter(HashSet<String> hospital) {
+        hospitals.addAll(hospital);
+    }
 
-	@Override
-	public void onBindViewHolder(@NonNull HospitalViewHolder holder, int position) {
-		holder.nameTextView.setText(hospitals[position]);
-	}
+    @NonNull
+    @Override
+    public HospitalViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.view_hospitals, parent, false);
+        return new HospitalViewHolder(view);
+    }
 
-	@Override
-	public int getItemCount() {
-		return hospitals.length;
-	}
+    @Override
+    public void onBindViewHolder(@NonNull HospitalViewHolder holder, int position) {
+        holder.nameTextView.setText(hospitals.get(position));
+    }
 
-	public static class HospitalViewHolder extends RecyclerView.ViewHolder {
-		private final TextView nameTextView;
+    @Override
+    public int getItemCount() {
+        return hospitals.size();
+    }
 
-		public HospitalViewHolder(@NonNull View itemView) {
-			super(itemView);
-			nameTextView = itemView.findViewById(R.id.name);
-		}
-	}
+    public static class HospitalViewHolder extends RecyclerView.ViewHolder {
+        private final TextView nameTextView;
+
+        public HospitalViewHolder(@NonNull View itemView) {
+            super(itemView);
+            nameTextView = itemView.findViewById(R.id.name);
+        }
+    }
 }
--- app/src/main/java/com/sjbit/ereport/main/ui/hospitals/HospitalsFragment.java
+++ app/src/main/java/com/sjbit/ereport/main/ui/hospitals/HospitalsFragment.java
@@ -1,6 +1,7 @@
 package com.sjbit.ereport.main.ui.hospitals;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,12 +12,28 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.sjbit.ereport.R;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.HashSet;
 
 /**
  * Handles the Hospitals View of the User.
  */
 public class HospitalsFragment extends Fragment {
+	private static final FirebaseAuth auth = FirebaseAuth.getInstance();
+	private static final FirebaseDatabase database = FirebaseDatabase.getInstance();
+
+	public static HashSet<String> hospitals= new HashSet<>();
 
 	public View onCreateView(@NonNull LayoutInflater inflater,
 							 ViewGroup container, Bundle savedInstanceState) {
@@ -26,12 +43,35 @@ public class HospitalsFragment extends Fragment {
 	@Override
 	public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
 		super.onViewCreated(view, savedInstanceState);
-
+		getHospitalFromFirebase();
 		//Setup Recycler View
 		RecyclerView hospitalsView = view.findViewById(R.id.hospitals_recycler_view);
 		GridLayoutManager manager = new GridLayoutManager(requireContext(), 2);
 		manager.setOrientation(RecyclerView.VERTICAL);
-		hospitalsView.setAdapter(new HospitalAdapter());
+		hospitalsView.setAdapter(new HospitalAdapter(hospitals));
 		hospitalsView.setLayoutManager(manager);
+	}
+
+	public static void getHospitalFromFirebase(){
+		FirebaseUser user = auth.getCurrentUser();
+		assert user != null;
+		DatabaseReference reference = database.getReference().child("users").child(user.getUid()).child("blockchain");
+		reference.addListenerForSingleValueEvent(new ValueEventListener() {
+			@Override
+			public void onDataChange(@NonNull @NotNull DataSnapshot snapshot) {
+				for(DataSnapshot snap:snapshot.getChildren()){
+					if(snap.child("blockObject").child("type").getValue().toString().equals("REPORT")){
+						hospitals.add(snap.child("blockObject").child("hospitalName").getValue().toString());
+					}
+				}
+				Log.i("Hospitals",hospitals.toString());
+			}
+
+			@Override
+			public void onCancelled(@NonNull @NotNull DatabaseError error) {
+
+			}
+		});
+
 	}
 }
